9e329fb73c8d80a0934c398e8d4459988773ce54
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-02-13 13:09:36 -0500

    added image field

--- productList.js
+++ productList.js
@@ -1,14 +1,14 @@
 (function () {
     var itemsDB = [
-        { id: '123', name: 'Article 1', published: true },
-        { id: '583', name: 'Article 2', published: true },
-        { id: '954', name: 'Article 3', published: false },
-        { id: '384', name: 'Article 4', published: false },
-        { id: '183', name: 'Article 5', published: true },
-        { id: '007', name: 'Article 6', published: false },
-        { id: '304', name: 'Article 7', published: true },
-        { id: '729', name: 'Article 8', published: false },
-        { id: '734', name: 'Article 9', published: true },
+        { id: '001', name: 'Item 1', price: 121, image: '', published: true },
+        { id: '002', name: 'Item 2', price: 122, image: '', published: true },
+        { id: '003', name: 'Item 3', price: 123, image: '', published: false },
+        { id: '004', name: 'Item 4', price: 124, image: '', published: false },
+        { id: '005', name: 'Item 5', price: 125, image: '', published: true },
+        { id: '006', name: 'Item 6', price: 126, image: '', published: false },
+        { id: '007', name: 'Item 7', price: 127, image: '', published: true },
+        { id: '008', name: 'Item 8', price: 128, image: '', published: false },
+        { id: '009', name: 'Item 9', price: 129, image: '', published: true },
     ];
     function renderList(results) {
         var items = document.querySelector('#items-container');
@@ -16,7 +16,7 @@
         var tableRows = results.map(function (result, index) {
             return `
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
+                <img class="card-img-top" src="${result.image}" alt="Card image cap">
                 <div class="card-body">
                     <p class="card-text">${result.name}</p>
                 </div>
