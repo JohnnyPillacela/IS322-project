f370ec25374aa7ff9783aeb1ea9e38a18fea95eb
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-02-13 17:30:08 -0500

    edited list items

--- productList.js
+++ productList.js
@@ -1,14 +1,14 @@
 (function () {
     var itemsDB = [
-        { id: '001', name: 'Item 1', price: 121, image: './images/M1ProMacBookPro.jpeg', published: true },
-        { id: '002', name: 'Item 2', price: 122, image: './images/iphone12.png', published: true },
-        { id: '003', name: 'Item 3', price: 123, image: './images/', published: false },
-        { id: '004', name: 'Item 4', price: 124, image: './images/', published: false },
-        { id: '005', name: 'Item 5', price: 125, image: './images/', published: true },
-        { id: '006', name: 'Item 6', price: 126, image: './images/', published: false },
-        { id: '007', name: 'Item 7', price: 127, image: './images/', published: true },
-        { id: '008', name: 'Item 8', price: 128, image: './images/', published: false },
-        { id: '009', name: 'Item 9', price: 129, image: './images/', published: true },
+        { id: '001', name: 'MacBook Pro, M1 Pro Chip', price: 121, image: './images/M1ProMacBookPro.jpeg', published: true },
+        { id: '002', name: 'iPhone 12', price: 122, image: './images/iphone12.png', published: true },
+        { id: '003', name: 'iPad Pro', price: 123, image: './images/iPadProM1.jpeg', published: false },
+        { id: '004', name: 'Air Pods 3rd Gen', price: 124, image: './images/airpods3.jpeg', published: false },
+        { id: '005', name: 'Samsung S21', price: 125, image: './images/S21.jpeg', published: true },
+        { id: '006', name: 'Samsung Galaxy Book', price: 126, image: './images/samsung-notebook.jpeg', published: false },
+        { id: '007', name: 'Apple Pencil', price: 127, image: './images/apple-pencil.jpeg', published: true },
+        { id: '008', name: 'Samsung Watch', price: 128, image: './images/samsungWatch.jpg', published: false },
+        { id: '009', name: 'Apple Home Pod', price: 129, image: './images/appleHomePod.jpeg', published: true },
     ];
     function renderList(results) {
         var items = document.querySelector('#items-container');
